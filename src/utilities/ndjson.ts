a3fc75d536b12985124a036db06bf6342bc16cde
Jia Jun Ngeo <jia.jun@besquare.com.my>
2022-08-08 08:28:59 +0800

    ndjson:added schema version support

--- src/utilities/ndjson.ts
+++ src/utilities/ndjson.ts
@@ -4,18 +4,24 @@
 
 import { createReadStream, createWriteStream } from "fs";
 
+export declare type NDJsonInfo = {
+  payload: any[];
+  version: number;
+};
 /**
  * Parse the NDJSon from a file
  * @param path path to the file in question
  * @returns parsed data
  */
-export function parseNDJson(path: string): Promise<any[]> {
+export function parseNDJson(path: string): Promise<NDJsonInfo> {
   let stream = createReadStream(path, { encoding: "utf-8" });
   let return_val: any[] = [];
   let buff = "";
+  let parsed_json = false;
   let failed = false;
+  let version = 1;
   let callback_resolve: Function, callback_reject: Function;
-  let return_promise: Promise<any[]> = new Promise((resolve, reject) => {
+  let return_promise: Promise<NDJsonInfo> = new Promise((resolve, reject) => {
     callback_resolve = resolve;
     callback_reject = reject;
   });
@@ -27,6 +33,15 @@ export function parseNDJson(path: string): Promise<any[]> {
         let buff_to_parse = buff.substring(0, idx).trim();
         //remove it from the buffer
         buff = buff.slice(idx + 1);
+        if (!parsed_json) {
+          parsed_json = true;
+          //check the presence of version number inside the response
+          if (/^[\d+]$/.test(buff_to_parse)) {
+            let parsed_version = parseInt(buff_to_parse);
+            version = parsed_version;
+            continue;
+          }
+        }
         if (buff_to_parse.length > 0) {
           //parse that and push it into return_val
           return_val.push(JSON.parse(buff_to_parse));
@@ -57,7 +72,10 @@ export function parseNDJson(path: string): Promise<any[]> {
       return;
     }
     if (!failed) {
-      callback_resolve(return_val);
+      callback_resolve({
+        payload: return_val,
+        version,
+      });
     }
   });
   return return_promise;
@@ -68,11 +86,17 @@ export function parseNDJson(path: string): Promise<any[]> {
  * @param path path to the file to write
  * @param data data to write
  */
-export function writeNDJson(path: string, data: any[]): Promise<void> {
+export function writeNDJson(
+  path: string,
+  data: any[],
+  version: number = 1
+): Promise<void> {
   return new Promise((resolve, reject) => {
     let stream = createWriteStream(path);
     stream.on("error", reject);
     stream.on("finish", resolve);
+    //write the version number
+    stream.write(`${version}\n`);
     data.forEach((e) => stream.write(`${JSON.stringify(e)}\n`));
     stream.end();
   });
