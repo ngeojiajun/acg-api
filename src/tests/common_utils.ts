02801587fd19b554f58360305aa670d33a1102e4
Junichi Fujiwara <fujiwarajunichi2000@outlook.jp>
2022-08-14 22:06:37 +0900

    added more assert operators

--- src/tests/common_utils.ts
+++ src/tests/common_utils.ts
@@ -9,3 +9,27 @@ export function assertSuccess(result: Status) {
     fail(result.message);
   }
 }
+
+export async function assertThrow(
+  f: () => void | Promise<void>,
+  tag: string = "Unknown"
+) {
+  try {
+    await f();
+  } catch (e) {
+    return;
+  }
+  fail(`The ${tag} didnt throw`);
+}
+
+export function assertMatch<T>(value: T, expected: T) {
+  if (value !== expected) {
+    fail(`Values didnt match! Expecting ${expected} but got ${value}`);
+  }
+}
+
+export function assertNotMatch<T>(value: T, rhs: T) {
+  if (value === rhs) {
+    fail(`Values does match while it is not supposed to be! Value= ${value}`);
+  }
+}
