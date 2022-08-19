53b252995c90164a310000a40625943c7f4d4fde
Jia Jun Ngeo <jia.jun@besquare.com.my>
2022-08-19 09:21:55 +0800

    added character api

--- src/api/characters.ts
+++ src/api/characters.ts
@@ -1,5 +1,5 @@
 import express, { Application, NextFunction, Request, Response } from "express";
-import { IDatabase } from "../database/database";
+import { Condition, IDatabase } from "../database/database";
 import { asCharacter } from "../definitions/converters";
 import { Character } from "../definitions/core";
 import { tryParseInteger } from "../utils";
@@ -14,6 +14,7 @@ export default class CharacterApi {
     const app = express();
     app.disable("x-powered-by");
     app.get("/all", this.#getCharacters.bind(this));
+    app.get("/search", this.#searchCharacters.bind(this));
     app.get("/:id", this.#getCharactersById.bind(this));
     app.use(nonExistantRoute);
     app.use(errorHandler);
@@ -94,6 +95,43 @@ export default class CharacterApi {
           response.status(200).type("json").json(result).end();
         }
       }
+    } catch (e) {
+      next(e);
+    }
+  }
+  /**
+   * Get search character
+   * @route /search
+   */
+  async #searchCharacters(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
+    try {
+      let { q } = request.query;
+      if (!q || typeof q !== "string" || q.trim().length === 0) {
+        response.status(400).json({ error: "Missing query string" }).end();
+        return;
+      }
+      let response_json = [];
+      for await (const key of this.#database.iterateKeysIf(
+        "ANIME",
+        null,
+        [
+          { key: "name", op: "INCLUDES_INSENSITIVE", rhs: q },
+          { key: "nameInJapanese", op: "INCLUDES_INSENSITIVE", rhs: q },
+        ] as Condition<Character, "INCLUDES_INSENSITIVE">[],
+        "OR"
+      )) {
+        //get every single anime entry
+        //note this one always success because the validation done
+        let entry = await this.#dbGetCharacterById(key);
+        if (entry) {
+          response_json.push(entry);
+        }
+      }
+      response.status(200).type("json").json(response_json).end();
     } catch (e) {
       next(e);
     }
