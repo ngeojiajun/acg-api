6255110379bbaa2b7d1fecb7155670cc3706ee0c
Junichi Fujiwara <fujiwarajunichi2000@outlook.jp>
2022-08-21 00:32:16 +0900

    added missing integrity test for manga

--- src/tests/integrity_test.ts
+++ src/tests/integrity_test.ts
@@ -2,10 +2,12 @@ import { IDatabase } from "../database/database";
 import {
   checkRemoteReferencesAnimeEntry,
   checkRemoteReferencesCharacter,
+  checkRemoteReferencesMangaEntry,
 } from "../database/integrityTestUtils";
 import JsonDatabase from "../database/jsonDatabase";
 import { AnimeEntryInternal } from "../definitions/anime.internal";
 import { Character } from "../definitions/core";
+import { MangaEntryInternal } from "../definitions/manga.internal";
 import { assertSuccess, fail } from "./common_utils";
 
 export async function IntegrityTest() {
@@ -23,6 +25,17 @@ export async function IntegrityTest() {
           "Key returned from iterator must be resolvable but it is not in fact"
         );
       assertSuccess(await checkRemoteReferencesAnimeEntry(db, a));
+    }
+    console.log("Anime table contain no dangling pointers");
+    //test every single pointer from manga
+    console.log("Checking manga table....");
+    for await (const k of db.iterateKeys("MANGA")) {
+      let a = await db.getData<MangaEntryInternal>("MANGA", k);
+      if (!a)
+        fail(
+          "Key returned from iterator must be resolvable but it is not in fact"
+        );
+      assertSuccess(await checkRemoteReferencesMangaEntry(db, a));
     }
     console.log("Anime table contain no dangling pointers");
     console.log("Checking...... characters table");
