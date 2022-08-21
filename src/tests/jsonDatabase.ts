07ccaa6dfb6bd40aec87ba1b435bb3f369d25907
Junichi Fujiwara <fujiwarajunichi2000@outlook.jp>
2022-08-21 23:08:25 +0900

    completed the test suite for json database

--- src/tests/jsonDatabase.ts
+++ src/tests/jsonDatabase.ts
@@ -5,6 +5,7 @@ import { AnimeEntryInternal } from "../definitions/anime.internal";
 import { Category, Character, People } from "../definitions/core";
 import { assertFail, assertSuccess } from "./common_utils";
 import "../utilities/prototype_patch";
+import { MangaEntryInternal } from "../definitions/manga.internal";
 
 const testCategory1: Category = {
   id: 1,
@@ -22,13 +23,28 @@ const testAnime1: AnimeEntryInternal = {
   category: [1],
 };
 
+const testManga1: MangaEntryInternal = {
+  id: 1,
+  name: "chicken",
+  nameInJapanese: "鶏",
+  year: 2000,
+  description: "yo",
+  isFinished: false,
+  author: [1],
+  publisher: [1],
+  category: [1],
+};
+
 const testCharacter: Character = {
   id: 0,
   name: "Haruka",
   nameInJapanese: "春華",
   description: "abv",
   gender: "female",
-  presentOn: [{ id: 1, type: "anime" }],
+  presentOn: [
+    { id: 1, type: "anime" },
+    { id: 1, type: "manga" },
+  ],
 };
 
 const testPerson: People = {
@@ -49,12 +65,17 @@ export async function JsonDatabaseTests(_temp: PathLike) {
       "Adding anime with dangling reference"
     );
     assertFail(
+      await database.addData("MANGA", testManga1),
+      "Adding manga with dangling reference"
+    );
+    assertFail(
       await database.addData("CHARACTER", testCharacter),
       "Adding character with dangling reference"
     );
     assertSuccess(await database.addData("PERSON", testPerson));
     assertSuccess(await database.addData("CATEGORY", testCategory1));
     assertSuccess(await database.addData("ANIME", testAnime1));
+    assertSuccess(await database.addData("MANGA", testManga1));
     assertSuccess(await database.addData("CHARACTER", testCharacter));
     console.log("Testing illegal operations: addData");
     assertFail(
@@ -70,6 +91,10 @@ export async function JsonDatabaseTests(_temp: PathLike) {
       "Adding duplicated data"
     );
     assertFail(
+      await database.addData("MANGA", testManga1),
+      "Adding duplicated data"
+    );
+    assertFail(
       await database.addData("CHARACTER", testCharacter),
       "Adding duplicated data"
     );
@@ -78,14 +103,17 @@ export async function JsonDatabaseTests(_temp: PathLike) {
     testCategory1.name = "123311";
     testCharacter.name = "123311";
     testAnime1.name = "123311";
+    testManga1.name = "12334";
     console.log("Adding another row");
     assertSuccess(await database.addData("PERSON", testPerson));
     assertSuccess(await database.addData("CATEGORY", testCategory1));
     assertSuccess(await database.addData("ANIME", testAnime1));
+    assertSuccess(await database.addData("MANGA", testManga1));
     assertSuccess(await database.addData("CHARACTER", testCharacter));
     console.log("Testing illegal operations: updateData");
     //undoing the changes
     testAnime1.name = "chicken";
+    testManga1.name = "chicken";
     testCategory1.name = "fish";
     testCharacter.name = "Haruka";
     testPerson.name = "hana";
@@ -93,6 +121,10 @@ export async function JsonDatabaseTests(_temp: PathLike) {
     //but it should be fails
     assertFail(
       await database.updateData("ANIME", 2, testAnime1),
+      "Update the data so it similar for another entry"
+    );
+    assertFail(
+      await database.updateData("MANGA", 2, testManga1),
       "Update the data so it similar for another entry"
     );
     assertFail(
@@ -110,6 +142,10 @@ export async function JsonDatabaseTests(_temp: PathLike) {
     //test the delete
     console.log("Testing the delete operations");
     assertFail(
+      await database.removeData("MANGA", 1),
+      "Removing data which referred by others"
+    );
+    assertFail(
       await database.removeData("ANIME", 1),
       "Removing data which referred by others"
     );
@@ -121,7 +157,13 @@ export async function JsonDatabaseTests(_temp: PathLike) {
       await database.removeData("PERSON", 1),
       "Removing data which referred by others"
     );
-    const types: DatabaseTypes[] = ["CHARACTER", "ANIME", "PERSON", "CATEGORY"];
+    const types: DatabaseTypes[] = [
+      "CHARACTER",
+      "ANIME",
+      "MANGA",
+      "PERSON",
+      "CATEGORY",
+    ];
     for (const type of types) {
       for (let i = 1; i < 3; i++) {
         assertSuccess(await database.removeData(type, i));
