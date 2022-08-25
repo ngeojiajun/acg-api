77a2dde2e47a5dfb64af293b0f96012523fd1439
Jia Jun Ngeo <jia.jun@besquare.com.my>
2022-08-25 01:01:26 +0000

    make the call more specific

--- src/utilities/hashing.ts
+++ src/utilities/hashing.ts
@@ -2,6 +2,6 @@ import { createHash } from "crypto";
 //Some internal APIs for the caching
 export function computeHashForObject(o: any): string {
   const hash = createHash("sha256");
-  hash.update(JSON.stringify(o));
+  hash.update(JSON.stringify(o, null, 0));
   return hash.digest("hex");
 }
